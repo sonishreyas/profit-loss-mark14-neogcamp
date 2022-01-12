eb2844808a3cbbfd4dca94bd2fd7692c996cf27d
Shreyas Soni <shreyas-soni@pluralsight.com>
2022-01-13 02:38:33 +0530

    ref: code upgraded to ES6+

--- app.js
+++ app.js
@@ -4,49 +4,55 @@ const currentPrice = document.querySelector("#current-price");
 const checkButton = document.querySelector("#check-stock");
 const output = document.querySelector("#output");
 
+const calculateInvestedAmount = () =>
+  Number(initialPrice.value) * Number(quantityStocks.value);
 
-function calculateInvestedAmount()  {
-    var invested = Number(initialPrice.value) * Number(quantityStocks.value);
-    return invested;
-}
+const calculateCurrentValue = () =>
+  Number(currentPrice.value) * Number(quantityStocks.value);
 
-function calculateCurrentValue()  {
-    var currentvalue = Number(currentPrice.value) * Number(quantityStocks.value);
-    return currentvalue;
-}
+const Percentage = (a, b) => ((a - b) / a) * 100;
 
-function Percentage(a,b)   {
-    return ((a-b)/a)*100
-}
+const calculateprofitloss = () => {
+  const currentvalue = calculateCurrentValue();
+  const invested = calculateInvestedAmount();
+  let message;
+  if (invested > currentvalue) {
+    message = `Whoops!! Your loss is ${
+      invested - currentvalue
+    } and loss Percentage is ${Percentage(invested, currentvalue).toFixed(2)}`;
+    output.style.color = "red";
+  } else if (invested < currentvalue) {
+    message = `Yay!! Your Profit is ${
+      currentvalue - invested
+    } and profit Percentage is ${Percentage(currentvalue, invested).toFixed(
+      2
+    )}`;
+    output.style.color = "green";
+  } else {
+    message = `No pain No gain, and no gain no pain :)`;
+    output.style.color = "white";
+  }
+  output.innerText = message;
+};
 
-function calculateprofitloss()   {
-    var currentvalue = calculateCurrentValue();
-    var invested = calculateInvestedAmount();
-    var message;
-    if(invested > currentvalue) {
-        message = "Whoops!! Your loss is "+ (invested-currentvalue) +" and loss Percentage is " + Percentage(invested,currentvalue).toFixed(2); 
-        output.style.color = 'red';
-    }   else if (invested < currentvalue)   {
-        message = "Yay!! Your Profit is "+ (currentvalue-invested) +" and profit Percentage is " + Percentage(currentvalue,invested).toFixed(2);
-        output.style.color = 'green';
-    } else{
-        message = "No pain No gain, and no gain no pain :)"
-        output.style.color = 'white';
+const submitHandler = () => {
+  if (
+    initialPrice.value != "" &&
+    quantityStocks.value != "" &&
+    currentPrice.value != ""
+  ) {
+    if (
+      Number(initialPrice.value) >= 0 &&
+      Number(quantityStocks.value) >= 0 &&
+      Number(currentPrice.value) >= 0
+    ) {
+      calculateprofitloss();
+    } else {
+      output.style.color = "white";
+      output.innerText = "Values cannot be negative";
     }
-    output.innerText = message;
-}
-
-function submitHandler()    {
-    if(initialPrice.value != "" && quantityStocks.value != "" && currentPrice.value != ""){
-        if(Number(initialPrice.value) >= 0 && Number(quantityStocks.value) >= 0 && Number(currentPrice.value) >= 0){       
-            calculateprofitloss();
-        }   else   {
-            output.style.color = 'white';
-            output.innerText = "Values cannot be negative";
-        }
-    }   else    {
-        alert('Please fill out all Fields');
-    }
-        
-}
-checkButton.addEventListener("click",submitHandler);
+  } else {
+    alert("Please fill out all Fields");
+  }
+};
+checkButton.addEventListener("click", submitHandler);
